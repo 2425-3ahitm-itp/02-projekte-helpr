14c9d04664ee8dc8fd7d5e111ba89eb7547a0309
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2024-11-12 18:21:40 +0100

    chore: add javafx project structure

---
+++ javafx/src/main/java/at/htl/javafx/App.java
@@ -0,0 +1,21 @@
+package at.htl.javafx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Scene scene = new Scene( new StackPane(), 320, 240 );
+        primaryStage.setTitle( "App" );
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ javafx/src/main/java/at/htl/javafx/AppLauncher.java
@@ -0,0 +1,7 @@
+package at.htl.javafx;
+
+public class AppLauncher {
+    public static void main( String[] args ) {
+        App.main( args );
+    }
+}
