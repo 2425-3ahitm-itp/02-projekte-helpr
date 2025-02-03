e65e08304efd3bc5dc49c611ca9f6e6bce67fcdd
Jakob Huemer-Fistelberger <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-02-03 11:26:33 +0100

    chore: format pom.xml and App.java

--- javafx/src/main/java/at/htl/helpr/App.java
+++ javafx/src/main/java/at/htl/helpr/App.java
@@ -9,13 +9,13 @@ import java.io.IOException;
 
 public class App extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start( Stage primaryStage ) throws IOException {
         Scene scene = new Scene( new StackPane(), 320, 240 );
         primaryStage.setTitle( "App" );
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         launch();
     }
 }
