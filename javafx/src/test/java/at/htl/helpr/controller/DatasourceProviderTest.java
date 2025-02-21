82188aaa928cbed59dd0c253d3b6e2c565a03d52
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-02-21 21:36:08 +0100

    test(datasourceprovider): remove test because class not used anymore

--- javafx/src/test/java/at/htl/helpr/controller/DatasourceProviderTest.java
+++ javafx/src/test/java/at/htl/helpr/controller/DatasourceProviderTest.java
@@ -1,49 +1,49 @@
-package at.htl.helpr.controller;
-
-import io.quarkus.test.junit.QuarkusTest;
-import jakarta.inject.Inject;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@QuarkusTest
-class DatasourceProviderTest {
-
-    @Inject
-    DataSource dataSource;
-
-    @BeforeEach
-    void setup() {
-        assertThat(dataSource).isNotNull();
-    }
-
-    @Test
-    void testDataSourceIsNotNull() {
-        assertThat(dataSource).isNotNull();
-    }
-
-    @Test
-    void testDataSourceIsValid() throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            assertThat(connection).isNotNull();
-            assertThat(connection.isValid(2)).isTrue();
-        }
-    }
-
-    @Test
-    void testDataSourceCanExecuteQuery() throws SQLException {
-        try (Connection connection = dataSource.getConnection();
-             var stmt = connection.createStatement();
-             var rs = stmt.executeQuery("SELECT 1")) {
-
-            assertThat(rs).isNotNull();
-            assertThat(rs.next()).isTrue();
-            assertThat(rs.getInt(1)).isEqualTo(1);
-        }
-    }
-}
+//package at.htl.helpr.controller;
+//
+//import io.quarkus.test.junit.QuarkusTest;
+//import jakarta.inject.Inject;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.BeforeEach;
+//
+//import javax.sql.DataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@QuarkusTest
+//class DatasourceProviderTest {
+//
+//    @Inject
+//    DataSource dataSource;
+//
+//    @BeforeEach
+//    void setup() {
+//        assertThat(dataSource).isNotNull();
+//    }
+//
+//    @Test
+//    void testDataSourceIsNotNull() {
+//        assertThat(dataSource).isNotNull();
+//    }
+//
+//    @Test
+//    void testDataSourceIsValid() throws SQLException {
+//        try (Connection connection = dataSource.getConnection()) {
+//            assertThat(connection).isNotNull();
+//            assertThat(connection.isValid(2)).isTrue();
+//        }
+//    }
+//
+//    @Test
+//    void testDataSourceCanExecuteQuery() throws SQLException {
+//        try (Connection connection = dataSource.getConnection();
+//             var stmt = connection.createStatement();
+//             var rs = stmt.executeQuery("SELECT 1")) {
+//
+//            assertThat(rs).isNotNull();
+//            assertThat(rs.next()).isTrue();
+//            assertThat(rs.getInt(1)).isEqualTo(1);
+//        }
+//    }
+//}
