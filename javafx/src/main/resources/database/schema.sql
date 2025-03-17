96be203997c67ff586d5b82eafd28e76dea06c60
JakobHuemer <j.huemer-fistelberger@htblaleonding.onmicrosoft.com>
2025-03-17 23:02:33 +0100

    chore: update schema.sql, make application cascade on delete/update, drop public schema on startup #helpr-23

--- javafx/src/main/resources/database/schema.sql
+++ javafx/src/main/resources/database/schema.sql
@@ -2,11 +2,13 @@
 -- pgModeler version: 1.1.6
 -- PostgreSQL version: 17.0
 -- Project Site: pgmodeler.io
--- Model Author: ---
+-- Model Author: Jakob
 
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
 
 -- object: public.u_user | type: TABLE --
-DROP TABLE IF EXISTS public.u_user CASCADE;
+-- DROP TABLE IF EXISTS public.u_user CASCADE;
 CREATE TABLE public.u_user (
 	user_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ,
 	username varchar(50) NOT NULL,
@@ -20,7 +22,7 @@ ALTER TABLE public.u_user OWNER TO helpr;
 -- ddl-end --
 
 -- object: public.task | type: TABLE --
-DROP TABLE IF EXISTS public.task CASCADE;
+-- DROP TABLE IF EXISTS public.task CASCADE;
 CREATE TABLE public.task (
 	task_id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ,
 	author_id bigint NOT NULL,
@@ -37,17 +39,17 @@ ALTER TABLE public.task OWNER TO helpr;
 -- ddl-end --
 
 -- object: author_u_user_fk | type: CONSTRAINT --
-ALTER TABLE public.task DROP CONSTRAINT IF EXISTS author_u_user_fk CASCADE;
+-- ALTER TABLE public.task DROP CONSTRAINT IF EXISTS author_u_user_fk CASCADE;
 ALTER TABLE public.task ADD CONSTRAINT author_u_user_fk FOREIGN KEY (author_id)
 REFERENCES public.u_user (user_id) MATCH FULL
 ON DELETE RESTRICT ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: public.application | type: TABLE --
-DROP TABLE IF EXISTS public.application CASCADE;
+-- DROP TABLE IF EXISTS public.application CASCADE;
 CREATE TABLE public.application (
-	user_id bigint,
-	task_id bigint,
+	user_id bigint NOT NULL,
+	task_id bigint NOT NULL,
 	created_at timestamp DEFAULT now()
 
 );
@@ -56,17 +58,17 @@ ALTER TABLE public.application OWNER TO helpr;
 -- ddl-end --
 
 -- object: u_user_fk | type: CONSTRAINT --
-ALTER TABLE public.application DROP CONSTRAINT IF EXISTS u_user_fk CASCADE;
+-- ALTER TABLE public.application DROP CONSTRAINT IF EXISTS u_user_fk CASCADE;
 ALTER TABLE public.application ADD CONSTRAINT u_user_fk FOREIGN KEY (user_id)
 REFERENCES public.u_user (user_id) MATCH FULL
 ON DELETE CASCADE ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: task_fk | type: CONSTRAINT --
-ALTER TABLE public.application DROP CONSTRAINT IF EXISTS task_fk CASCADE;
+-- ALTER TABLE public.application DROP CONSTRAINT IF EXISTS task_fk CASCADE;
 ALTER TABLE public.application ADD CONSTRAINT task_fk FOREIGN KEY (task_id)
 REFERENCES public.task (task_id) MATCH FULL
-ON DELETE SET NULL ON UPDATE CASCADE;
+ON DELETE CASCADE ON UPDATE CASCADE;
 -- ddl-end --
 
 
