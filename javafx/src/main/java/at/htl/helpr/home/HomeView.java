abe83b8de8b726e9ca0dcd45dd2878941fadb8c8
SimoneSperrer <s.sperrer@htblaleonding.onmicrosoft.com>
2025-04-26 16:57:26 +0200

    Adding the new filter sidebar #helpr-27 In Progress

--- javafx/src/main/java/at/htl/helpr/home/HomeView.java
+++ javafx/src/main/java/at/htl/helpr/home/HomeView.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -16,6 +17,25 @@ import javafx.scene.text.Font;
 public class HomeView extends BorderPane {
 
     private final GridPane cardGrid;
+
+    private final Button filterButton;
+    private final HBox paymentBox;
+    private final TextField minPaymentField;
+    private final TextField maxPaymentField;
+    private final HBox effortBox;
+    private final TextField effortField;
+    private final ToggleButton effortToggle;
+    private final HBox postalCodeBox;
+    private final TextField postalCodeField;
+    private final ToggleButton postalToggle;
+    private final HBox cityBox;
+    private final TextField cityField;
+    private final ToggleButton cityToggle;
+    private final HBox dateBox;
+    private final VBox dateFields;
+    private final TextField fromDateField;
+    private final TextField toDateField;
+
     private final Button searchButton;
     private final TextField searchField;
     private final Circle profilePicture;
@@ -30,7 +50,68 @@ public class HomeView extends BorderPane {
         sidebar.setPrefWidth(200);
         sidebar.setStyle("-fx-background-color: #f8f8f8; -fx-border-color: #d3d3d3;");
 
+// Filter Anwenden Button
+        filterButton = createStyledButton("Filter Anwenden", true);
 
+// Bezahlung Filter
+        Label paymentLabel = new Label("Bezahlung:");
+        paymentBox = new HBox(5);
+        minPaymentField = new TextField();
+        minPaymentField.setPromptText("MIN");
+        minPaymentField.setPrefWidth(80);
+        maxPaymentField = new TextField();
+        maxPaymentField.setPromptText("MAX");
+        maxPaymentField.setPrefWidth(80);
+        paymentBox.getChildren().addAll(minPaymentField, maxPaymentField);
+
+// Aufwand Filter
+        Label effortLabel = new Label("Aufwand:");
+        effortBox = new HBox(5);
+        effortField = new TextField();
+        effortField.setPrefWidth(170);
+        effortToggle = new ToggleButton();
+        effortToggle.setPrefWidth(20);
+        effortBox.getChildren().addAll(effortField, effortToggle);
+
+// PLZ Filter
+        Label postalCodeLabel = new Label("PLZ:");
+        postalCodeBox = new HBox(5);
+        postalCodeField = new TextField();
+        postalCodeField.setPrefWidth(170);
+        postalToggle = new ToggleButton();
+        postalToggle.setPrefWidth(20);
+        postalCodeBox.getChildren().addAll(postalCodeField, postalToggle);
+
+// Ort Filter
+        Label cityLabel = new Label("Ort:");
+        cityBox = new HBox(5);
+        cityField = new TextField();
+        cityField.setPrefWidth(170);
+        cityToggle = new ToggleButton();
+        cityToggle.setPrefWidth(20);
+        cityBox.getChildren().addAll(cityField, cityToggle);
+
+// Erstelldatum Filter
+        Label creationDateLabel = new Label("Erstelldatum:");
+        dateBox = new HBox(5);
+        dateFields = new VBox(5);
+        fromDateField = new TextField();
+        fromDateField.setPromptText("von");
+        toDateField = new TextField();
+        toDateField.setPromptText("bis");
+        dateFields.getChildren().addAll(fromDateField, toDateField);
+
+        dateBox.getChildren().addAll(dateFields);
+
+// Alles zusammensetzen
+        sidebar.getChildren().addAll(
+                filterButton,
+                paymentLabel, paymentBox,
+                effortLabel, effortBox,
+                postalCodeLabel, postalCodeBox,
+                cityLabel, cityBox,
+                creationDateLabel, dateBox
+        );
 
         // --- Suchleiste mit Profilbild & Username (Oben) ---
         HBox searchBar = new HBox(15);
