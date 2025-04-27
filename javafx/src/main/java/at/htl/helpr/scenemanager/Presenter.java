befd34388866f3529bff3c91587922683f94540b
mGnadlinger <m.gnadlinger@students.htl-leonding.ac.at>
2025-04-27 16:35:40 +0200

    feat: add abstract class presenter #helpr-31 In Progress

--- javafx/src/main/java/at/htl/helpr/scenemanager/Presenter.java
+++ javafx/src/main/java/at/htl/helpr/scenemanager/Presenter.java
@@ -1,4 +1,18 @@
 package at.htl.helpr.scenemanager;
 
-public class Presenter {
+import javafx.scene.Scene;
+
+public abstract class Presenter {
+
+    protected SceneManager sceneManager;
+    protected Object view;
+    protected Object model;
+
+    public Presenter(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+    }
+
+    public abstract Scene getScene();
+    public abstract void onShow();
+    public abstract void onHide();
 }
--- javafx/src/main/java/at/htl/helpr/scenemanager/SceneManager.java
+++ javafx/src/main/java/at/htl/helpr/scenemanager/SceneManager.java
@@ -1,5 +1,33 @@
 package at.htl.helpr.scenemanager;
 
+import javafx.stage.Stage;
+import java.util.HashMap;
+import java.util.Map;
+
 public class SceneManager {
 
+    private Stage primaryStage;
+    private Map<Class<? extends Presenter>, Presenter> presenters = new HashMap<>();
+    Presenter currentPresenter;
+
+
+    public SceneManager(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+    public void addPresenter(Class<? extends Presenter> presenterClass, Presenter presenter) {
+        presenters.put(presenterClass, presenter);
+    }
+
+    public void setScene(Class<? extends Presenter> presenterClass) {
+        Presenter nextPresenter = presenters.get(presenterClass);
+
+        if (currentPresenter != null) {
+            currentPresenter.onHide();
+        }
+
+        primaryStage.setScene(nextPresenter.getScene());
+        currentPresenter = nextPresenter;
+        currentPresenter.onShow();
+    }
 }
